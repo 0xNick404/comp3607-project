c850e184337b987c78554ff35518757ccfcbbebd
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-11-24 20:50:55 -0400

    Delete logging directory

--- logging/EventListener.java
+++
@@ -1,5 +0,0 @@
-package com.jeopardy.logging;
-
-public interface EventListener {
-    void onEvent(EventRecord event);
-}
--- logging/EventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.jeopardy.logging;
-
-public interface EventPublisher {
-    void registerListener(EventListener listener);
-
-    void unregisterListener(EventListener listener);
-
-    void publish(EventRecord event);
-}
