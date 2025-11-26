bfb65be2d4ecbf2f52c1ac24cc0ad9ac9b2b4623
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-11-26 17:35:17 -0400

    Update ParserFactoryTest.java

--- src/test/java/com/example/parser/ParserFactoryTest.java
+++ src/test/java/com/example/parser/ParserFactoryTest.java
@@ -1,7 +1,6 @@
 package com.example.parser;
 
 import com.example.parser.factory.ParserFactory;
-
 import com.example.parser.strategy.FileParserStrategy;
 import com.example.parser.strategy.CSVFileParser;
 import com.example.parser.strategy.JSONFileParser;
@@ -9,6 +8,12 @@ import com.example.parser.strategy.XMLFileParser;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for ParserFactory class.
+ * Tests that correct parser instances are returned for different file types.
+ * 
+ * @author Mahaveer Ragbir
+ */
 public class ParserFactoryTest {
 
     @Test
