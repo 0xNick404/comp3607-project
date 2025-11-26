86a0e3d7fe6ae6de2fff2dab2f64302284e4b995
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-11-26 00:12:33 -0400

    Refactor AskQuestionState to include logging

--- src/main/java/com/example/gameplay/state/AskQuestionState.java
+++ src/main/java/com/example/gameplay/state/AskQuestionState.java
@@ -5,7 +5,7 @@ import com.example.model.Question;
 /**
  * 
  */
-public class AskQuestionState implements GameState{
+public class AskQuestionState implements GameState {
     @Override
     public void loadGameState(GameEngine gameEngine) {
         Question q = gameEngine.getCurrentQuestion();
@@ -14,14 +14,24 @@ public class AskQuestionState implements GameState{
         System.out.println("Question: " + q.getQuestionText());
 
         String[] options = q.getOptions();
-        String[] labels = {"A", "B", "C", "D"};
+        String[] labels = { "A", "B", "C", "D" };
         for (int i = 0; i < options.length; i++) {
             System.out.println(labels[i] + ") " + options[i]);
         }
+
+        // Log that the question is being asked
+        gameEngine.publishEvent(
+                gameEngine.getCurrentPlayer().getName(),
+                "ASK_QUESTION",
+                q.getCategory(),
+                q.getValue(),
+                null,
+                null,
+                gameEngine.getCurrentPlayer().getScore());
     }
-    
+
     @Override
-    public void nextGameState(GameEngine gameEngine) {  
+    public void nextGameState(GameEngine gameEngine) {
         gameEngine.setGameState(new AcceptAnswerState());
     }
 }
