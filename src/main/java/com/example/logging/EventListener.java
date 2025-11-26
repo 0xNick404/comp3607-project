023798e4a7fba4af9591f8b528979f90882d6269
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-11-26 11:13:40 -0400

    Enhance EventListener with Javadoc comments
    Added documentation to EventListener interface.

--- src/main/java/com/example/logging/EventListener.java
+++ src/main/java/com/example/logging/EventListener.java
@@ -1,6 +1,18 @@
 package com.example.logging;
 
+/**
+ * Interface for objects that listen to and respond to event records.
+ * Implementations of this interface define how events are handled,
+ * such as logging to files, storing in memory, or triggering other actions.
+ * 
+ * @author Mahaveer Ragbir
+ */
 public interface EventListener {
+    /**
+     * Called when an event is published.
+     * Implementations should define the specific action to take when receiving an event.
+     * 
+     * @param event the event record containing details about the event
+     */
     void onEvent(EventRecord event);
-
 }
