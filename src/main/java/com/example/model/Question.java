42967d6d983db12cdf493480050f3fe378557c1f
veerragbir <115950138+veerragbir@users.noreply.github.com>
2025-11-26 17:32:21 -0400

    Update Question.java

--- src/main/java/com/example/model/Question.java
+++ src/main/java/com/example/model/Question.java
@@ -1,11 +1,12 @@
 package com.example.model;
 
 /**
- * Represents a question with a category, value, question text, multiple choice answer options, and correct answer.
+ * Represents a question with a category, value, question text, multiple choice
+ * answer options, and correct answer.
  * Tracks whether the question has been selected or not.
  */
 
-public class Question{
+public class Question {
     private String category;
     private int value;
     private String questionText;
@@ -13,14 +14,13 @@ public class Question{
     private String correctAnswer;
     private boolean hasBeenPicked;
 
-    public Question(String category, String value, String questionText, String[] options, String correctAnswer){
+    public Question(String category, String value, String questionText, String[] options, String correctAnswer) {
         this.category = category;
 
-        try{
+        try {
             this.value = Integer.parseInt(value);
-        }
-        catch(NumberFormatException e){
-            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            this.value = 0;
         }
 
         this.questionText = questionText;
@@ -29,12 +29,31 @@ public class Question{
         this.hasBeenPicked = false;
     }
 
-    public String getCategory(){return this.category;}
-    public int getValue(){return this.value;}
-    public String getQuestionText(){return this.questionText;}
-    public String[] getOptions(){return this.options;}
-    public String getCorrectAnswer(){return this.correctAnswer;}
-    public boolean hasBeenPicked(){return this.hasBeenPicked;}
+    public String getCategory() {
+        return this.category;
+    }
 
-    public void markPicked(){this.hasBeenPicked = true;}
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getQuestionText() {
+        return this.questionText;
+    }
+
+    public String[] getOptions() {
+        return this.options;
+    }
+
+    public String getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
+    public boolean hasBeenPicked() {
+        return this.hasBeenPicked;
+    }
+
+    public void markPicked() {
+        this.hasBeenPicked = true;
+    }
 }
